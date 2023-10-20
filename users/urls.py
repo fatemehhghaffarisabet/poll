839b8808c9902a7799dc564ae74268ba41508f49
Fatemeh Ghaffari Sabet <fatemehh.ghaffarisabet@gmail.com>
2023-10-20 15:13:30 +0330

    Change password added.

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from users.views import Signup, Login, Logout
+from users.views import Signup, Login, Logout, Change_password
 
 app_name = 'users'
 
 urlpatterns = [
     path('signup/', Signup.as_view(), name='signup'),
     path('login/', Login.as_view(), name='login'),
-    path('logout/', Logout.as_view(), name='logout')
+    path('logout/', Logout.as_view(), name='logout'),
+    path('change_password/', Change_password.as_view(), name='change_password')
 ]
--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth import login, logout
 from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 
 
@@ -42,3 +43,19 @@ class Logout(View):
     def get(self, request):
         logout(request)
         return HttpResponseRedirect(reverse('landing:home'))
+    
+class Change_password(LoginRequiredMixin, View):
+    def get(self, request):
+        return render(request, 'users/change_password.html')
+    
+    def post(self, request):
+        data = request.POST.dict()
+        old_password, new_password, confirm_password = data.get('old_password'), data.get('new_password'), data.get('confirm_password')
+        if request.user.check_password(old_password) and new_password == confirm_password:
+            request.user.set_password(new_password)
+            request.user.save()
+            return HttpResponseRedirect(reverse('landing:home'))
+        elif password != confirm_password:
+            return render(request, 'users/change_password.html', context={'error': 'dos not math'})
+        else:
+            return render(request, 'users/change_password.html', context={'error': 'password is wrong'})
